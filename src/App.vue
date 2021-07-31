1ab43f8e9985304dbaf8e24acd4b9d43ddfa7478
ahmadfarhanstwn <ahmadfarhanstwn@gmail.com>
2021-07-31 20:28:58 +0700

    change a bit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,4 @@
 <template>
-  <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <router-link :to="{ name: 'login' }" class="navbar-brand"
-      >VUELARA</router-link
-    >
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item">
-          <router-link :to="{ name: 'register' }" class="nav-link"
-            >REGISTER</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'login' }" class="nav-link"
-            >LOGIN</router-link
-          >
-        </li>
-      </ul>
-    </div>
-  </nav> -->
   <router-view></router-view>
 </template>
 
